85b888eff7558cc72cac4b9a5af4d5089ab4247e
ins-lu3fer <vivek.dnr1234@gmail.com>
2026-02-19 14:36:47 +0000

    operations

---
+++ 01_basics/05_operations.js
@@ -0,0 +1,23 @@
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello"
+let str2 = "vivek"
+
+let str3= str1 + str2
+//console.log(str3);
+
+let gameCounter = 100
+gameCounter++;
+//console.log(gameCounter)
+
+console.log("2">1);
+console.log(null>0);
+console.log(null == 0);
+console.log(null>=0);
