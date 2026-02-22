9de77b11136d6a71d074597f381df127093bcbad
ins-lu3fer <vivek.dnr1234@gmail.com>
2026-02-22 09:55:07 +0000

    date and time

---
+++ 01_basics/08_date.js
@@ -0,0 +1,28 @@
+/* console.log()
+ let myDate = new Date()
+ console.log(myDate.toString());
+ console.log(myDate.toDateString());
+ console.log(myDate.toLocaleString());
+ console.log(typeof myDate); */
+ 
+
+ let myCreatedDate = new Date("02-22-2026")
+ //console.log(myCreatedDate.toLocaleString());
+
+ let myTimeStamp = Date.now()
+ //console.log(myTimeStamp);
+ //console.log(myCreatedDate.getTime());
+ //console.log(Date.now()/1000);
+ let newDate = new Date()
+ console.log(newDate.getDay());
+ console.log(newDate.getMonth());
+
+ newDate.toLocaleString('deafult',{
+    weekday: "long",
+ })
+ 
+ 
+ 
+
+ 
+ 
