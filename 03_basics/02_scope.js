c5b54613da80945d7c28c08a989afa188e79e2ec
ins-lu3fer <vivek.dnr1234@gmail.com>
2026-03-05 08:32:47 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,32 @@
+function one(){
+    const username = "luci"
+    function two(){
+        const website ="youtube"
+        console.log(username);
+        
+    }
+   // console.log(website);
+    two()
+}
+//one()
+if(true){
+    const username ="lakshman prasad"
+    if (username=="lakshman prasad"){
+        const website = " youtube"
+        //console.log(username + website);
+    }
+}
+///////////+++++++ interesting ++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num +1
+}
+
+
+addTwo(5) //hoisting
+
+const addTwo = function(num){
+    return num +2
+}
+
